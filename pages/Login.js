e8e0f29d11ca2a637a9888096241487f9b07d4ef
moorels <sales@micropls.com>
2021-07-25 11:53:42 +1000

    rrr

---
+++ pages/Login.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { Navbar } from '../Components/Navbar'
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <Navbar/>
+            Login
+        </div>
+    )
+}
+
+export default Login
